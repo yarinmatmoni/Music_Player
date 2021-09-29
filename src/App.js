7225b69bc67fde82afe3e685789d3d3be410c892
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-09-29 15:52:00 +0300

    Add data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./styles/app.scss";
 // Adding components 
 import Player from './components/Player';
 import Song from './components/Song';
+//Import Data
+import data from "./data";
 
 function App() {
   return (
