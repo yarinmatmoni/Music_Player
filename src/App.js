1ad82458b54341f7130bef5f4fe714df6983529f
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-09-30 13:07:38 +0300

    Add state and add first style for current song

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./styles/app.scss";
 // Adding components 
 import Player from './components/Player';
@@ -7,10 +7,12 @@ import Song from './components/Song';
 import data from "./data";
 
 function App() {
+  const [songs,setSongs] = useState(data());
+  const [currentSong,setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song/>
-      <Player/>
+      <Song currentSong={currentSong} />
+      <Player />
     </div>
   );
 }
