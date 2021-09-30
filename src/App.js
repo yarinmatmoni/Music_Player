d16da33af2f615e05645dbb408f8008ac66f6ea5
Yarin Matmoni <yarinmatmoni@cs.colman.ac.il>
2021-09-30 13:53:03 +0300

    Add play and pause

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import Song from './components/Song';
 import data from "./data";
 
 function App() {
+  //State
   const [songs,setSongs] = useState(data());
   const [currentSong,setCurrentSong] = useState(songs[0]);
+  const [isPlaying,setIsPlaying] = useState(false);
+
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}  />
     </div>
   );
 }
